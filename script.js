62d140d2085226fc7d47983a4c232cc01a1468c4
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-22 16:54:16 +0100

    inserite condizioni "first" e "last" per non interrompere slideshow.

--- script.js
+++ script.js
@@ -2,10 +2,20 @@
 function prevClick(){
 
   var activeImg = $(".active");
-  var nextImg = activeImg.prev(".img-container");
 
-  activeImg.removeClass("active");
-  nextImg.addClass("active");
+  if(activeImg.hasClass("first")){
+
+    var prevImg = $(".img-container.last");
+    activeImg.removeClass("active");
+    prevImg.addClass("active");
+
+  } else {
+
+    var prevImg = activeImg.prev(".img-container");
+    activeImg.removeClass("active");
+    prevImg.addClass("active");
+
+  }
 
 }
 
@@ -13,12 +23,18 @@ function prevClick(){
 function nextClick(){
 
   var activeImg = $(".active");
-  var nextImg = activeImg.next(".img-container");
 
-  activeImg.removeClass("active");
-  nextImg.addClass("active");
+  if(activeImg.hasClass("last")){
+    var nextImg = $(".img-container.first");
+    activeImg.removeClass("active");
+    nextImg.addClass("active");
 
+  } else {
+    var nextImg = activeImg.next(".img-container");
+    activeImg.removeClass("active");
+    nextImg.addClass("active");
 
+  }
 
 }
 
