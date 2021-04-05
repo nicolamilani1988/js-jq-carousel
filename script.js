3ed0679b08a5823f5e5175440205b5dd5a9e767b
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-05 14:33:23 +0200

    messe funzioni per mostrare foto corispondente al click

--- script.js
+++ script.js
@@ -38,12 +38,42 @@ function nextClick(){
 
 }
 
+// funzione per colorare quadratino cliccato
+function viewPic(){
+  const clickedBox = $(this);
+  // console.log(clickedBox);
+  $(".point").removeClass("active");
+  clickedBox.addClass("active");
+  const activeElement = $(".point.active");
+  const classList = ["pic-1","pic-2","pic-3","pic-4"];
+
+  showPic(activeElement,classList);
+
+}
+
+// funzione per mostrare foto corrispondente
+function showPic(elem,array){
+
+  $(".img-container").removeClass("active");
+
+  for(let i = 0;i<array.length;i++){
+    let myClass = array[i];
+    if(elem.hasClass(myClass)){
+      $(`.${myClass}`).addClass("active");
+    }
+  
+  }
+
+}
+  
+
 // funzione avviamento
 function init(){
 
   $("#left").click(prevClick);
   $("#right").click(nextClick);
-
+  $(".point").click(viewPic);
+  
 }
 
-$(document).ready(init);
+$(init);
